a9da9b4cd4e9405313b6ee242958572be2290080
OlenaKorbut83 <olenakorbut83@gmail.com>
2024-12-11 21:49:14 +0200

    homework done

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,1 +1,14 @@
+export function searchImage(query) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const params = new URLSearchParams({
+    key: '47588447-b6614111152463e9a843ad075',
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
 
+  const url = `${BASE_URL}?${params}`;
+
+  return fetch(url).then(res => res.json());
+}
